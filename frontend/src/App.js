f29dc2817d120b5fa1dc820410fce396c891d6f3
Matisse Landais <matisse.landais@gmail.com>
2020-03-25 19:31:15 +0100

    Initial commit
    frontend/ and backend/ folders using react/express,
    Cors install to call backend/ api, material UI with tabbed pane set
    Basic call from frontend using simple route defined in backend.

---
+++ frontend/src/App.js
@@ -0,0 +1,137 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
+    </Typography>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export default function App() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+  const [apiState, setApiState] = React.useState("")
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  async function getApiState() {
+    const res = await fetch("http://localhost:9000/testAPI")
+    const data = await res.text()
+    setApiState(data);
+  }
+
+  useEffect(() => {
+    getApiState()
+  }, []);
+
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        <h1>Welcome to easy cooking ! </h1>
+        <p>Message from API : {apiState}</p>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
+    </div>
+  );
+}
+
+// import React, { Component } from 'react';
+// import './App.css';
+// import Tabs from '@material-ui/core/Tabs';
+// import Tab from '@material-ui/core/Tab'
+// import AppBar from '@material-ui/core/AppBar';
+
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       apiResponse: ""
+//     };
+//   }
+
+//   callAPI() {
+//     fetch("http://localhost:9000/testAPI")
+//       .then(res => res.text())
+//       .then(res => this.setState({ apiResponse: res }))
+//   }
+
+//   componentDidMount() {
+//     this.callAPI();
+//   }
+
+//   render() {
+//     return(
+//     <>
+//     <AppBar position="static">
+//       <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+//         <Tab label="Item One" {...a11yProps(0)} />
+//         <Tab label="Item Two" {...a11yProps(1)} />
+//         <Tab label="Item Three" {...a11yProps(2)} />
+//       </Tabs>
+//     </AppBar>
+//       <TabPanel value={value} index={0}>
+//         Item One
+//     </TabPanel>
+//           <TabPanel value={value} index={1}>
+//             Item Two
+//     </TabPanel>
+//           <TabPanel value={value} index={2}>
+//             Item Three
+//     </TabPanel>
+//     </>
+//     )
+//   }
+// }
