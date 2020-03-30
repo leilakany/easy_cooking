7c5b6f1885d948cc532bc8da5e9b90b46294628d
Leila <leila.kany@gmail.com>
2020-03-30 12:51:47 +0200

    Display 10 recipes when press enter
    TODO :
    - resize images & titles
    - error message when nothing is returned
    - select diet (ex:vegetarian)
    - display complete recipe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
+import { fade, makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
+import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
+import RecipeCard from './RecipeCard';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -42,21 +47,71 @@ const useStyles = makeStyles(theme => ({
     flexGrow: 1,
     backgroundColor: theme.palette.background.paper,
   },
-}));
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    }
+}}));
+
 
 export default function App() {
   const classes = useStyles();
+  const [state, setState] = useState({'recipes':[]});
   const [value, setValue] = React.useState(0);
-  const [apiState, setApiState] = React.useState("")
+  const [apiState, setApiState] = React.useState("");
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   async function getApiState() {
-    const res = await fetch("http://localhost:9000/testAPI")
+    const res = await fetch("http://localhost:9000/testAPI")  
     const data = await res.text()
-    setApiState(data);
+    setApiState(data);  
+  }
+
+  const keyPress = (event) => {
+    if(event.keyCode === 13){
+      axios.get('https://api.spoonacular.com/recipes/search?number=10&apiKey=d98ea6c059fb41daab9c7d8f751e086f&query='+event.target.value).then((resp) => {
+            let recipes = []
+            let url = resp.data.baseUri;
+            recipes = resp.data.results;
+            console.log(recipes)
+            setState({ 'recipes' : recipes, 'url':url});
+        })
+      }
   }
 
   useEffect(() => {
@@ -75,7 +130,29 @@ export default function App() {
       </AppBar>
       <TabPanel value={value} index={0}>
         <h1>Welcome to easy cooking ! </h1>
-        <p>Message from API : {apiState}</p>
+        <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <div>
+            <InputBase
+              placeholder="Search recipe"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+              onKeyDown = {keyPress}
+            />
+            </div>
+          </div>
+
+          <div>
+          <Grid container spacing={5}>  
+            {state.recipes.map((item, i)=>{ return <Grid spacing={3}><RecipeCard key={item.id} title={item.title} img={item.image} url={state.url}></RecipeCard></Grid>})}
+          </Grid>
+          </div>
+
       </TabPanel>
       <TabPanel value={value} index={1}>
         Item Two
