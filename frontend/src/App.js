fffae1e5105710cf54b0aefd1eaded781cbf043f
Leila <leila.kany@gmail.com>
2020-04-03 12:47:28 +0200

    separate cards

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -207,7 +207,7 @@ export default function App() {
         </FormControl>
         </Grid>
         <div className={classes.root}>
-        <Grid container direction="row"> 
+        <Grid container direction="row" spacing={3} >
               {state.recipes.map((item, i)=>{ return <Grid item xs={3} key={item.id}><RecipeCard title={item.title} img={item.image} url={state.url}></RecipeCard></Grid>})}
         </Grid>
         </div>
